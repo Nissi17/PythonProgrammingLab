89a8cd3b3b502c551d9df4a5527eea2e15422858
Nissi17 <nissi200527@gmail.com>
2025-03-21 15:39:55 +0530

    Compundinterest.py

---
+++ Compundinterest.py
@@ -0,0 +1,9 @@
+
+ # Code for compound interest
+ 
+ p=float(input("Enter the principal amount : "))
+ r=float(input("Enter rate of intrest : "))
+ t=int(input("Enter the years : "))
+ ci=p*((1+(r/100))**t)-p
+ 
+ print(ci)
