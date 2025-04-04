c56c3f14088f4f4cf86c7047c2cca701eb978f0c
Nissi17 <nissi200527@gmail.com>
2025-04-04 12:15:31 +0530

    pythonbankbuild.py

--- week3/pythonbankbuild.py
+++ week3/pythonbankbuild.py
@@ -1,18 +1,19 @@
-def credit(balance, balancex):
-    balance=balance + balancex
+def credit(balance, amount):
+    balance=balance + amount
     return balance
 
-def debit(balance,balancex):
-    balance=balance-balancex
+def debit(balance,amount):
+    balance=balance-amount
 
 def showBalance(balance):
     print(balance)
 
 
-    balancex=input("Enter the credit amount")
-    balancey=input("Enter the debit amount")
+    amount=int(input("Enter the credit amount"))
+    amount=int(input("Enter the debit amount"))
     balance=0
 
     balance =credit(2000,balancex)
     balance= debit(300,balancey)
-    print(balance)
+    showBalance(balance)
+
